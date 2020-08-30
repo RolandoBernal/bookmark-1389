7b4f2f79173afd41499991ccfb366e94d61ff695
rolandobernal <jrolandobernal@gmail.com>
2020-08-30 00:57:12 -0500

    Updates

---
+++ precache-manifest.b46c58ada8f92d330d5004c2904b526c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a432b0283773fac72ebb80a6ca0ef0",
+    "url": "/bookmark-1389/index.html"
+  },
+  {
+    "revision": "5ca0a5155a83bdaa912f",
+    "url": "/bookmark-1389/static/css/main.86b7fdf8.chunk.css"
+  },
+  {
+    "revision": "96d3a3d2e26fab0c1bac",
+    "url": "/bookmark-1389/static/js/2.4bd6b2e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bookmark-1389/static/js/2.4bd6b2e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca0a5155a83bdaa912f",
+    "url": "/bookmark-1389/static/js/main.64368eb6.chunk.js"
+  },
+  {
+    "revision": "a0bcc0a073bd3d051f84",
+    "url": "/bookmark-1389/static/js/runtime-main.b8c72f42.js"
+  }
+]);
